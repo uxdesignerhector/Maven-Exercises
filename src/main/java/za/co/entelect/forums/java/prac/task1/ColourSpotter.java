1f699ad17e55592a4b476a1a905f4c30aae5d5b1
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-07-08 21:08:59 +0200

    Added answer for exercise 1 (ColourSpotter).

--- src/main/java/za/co/entelect/forums/java/prac/task1/ColourSpotter.java
+++ src/main/java/za/co/entelect/forums/java/prac/task1/ColourSpotter.java
@@ -1,5 +1,15 @@
 package za.co.entelect.forums.java.prac.task1;
 
+import za.co.entelect.forums.java.example.App;
+import za.co.entelect.forums.java.example.domain.Color;
+import za.co.entelect.forums.java.example.domain.Vehicle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class ColourSpotter {
 
     /*
@@ -9,4 +19,33 @@ public class ColourSpotter {
         App.getGantryA();
      */
 
+    public static void main(String[] args) {
+        List<Vehicle> vehicles = App.getGantryA().getVehicles();
+        Map<Color, List<Vehicle>> vehiclesByColor = groupVehiclesByColor(vehicles);
+        printColorReport(vehiclesByColor);
+    }
+
+    private static Map<Color, List<Vehicle>> groupVehiclesByColor(List<Vehicle> vehicles) {
+        Map<Color, List<Vehicle>> vehiclesByColor = new HashMap<>();
+
+        for (Vehicle vehicle : vehicles) {
+            Color color = vehicle.getBodyColour();
+            List<Vehicle> vehiclesOfColor = vehiclesByColor.get(color);
+
+            if (vehiclesOfColor == null) {
+                vehiclesOfColor = new ArrayList<>();
+                vehiclesByColor.put(color, vehiclesOfColor);
+            }
+
+            vehiclesOfColor.add(vehicle);
+        }
+
+        return vehiclesByColor;
+    }
+
+    private static void printColorReport(Map<Color, List<Vehicle>> vehiclesByColor) {
+        for (Color color : vehiclesByColor.keySet()) {
+            System.out.println(color + ": " + vehiclesByColor.get(color).size());
+        }
+    }
 }
