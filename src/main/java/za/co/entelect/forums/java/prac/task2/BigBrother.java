77dde2cf4d17ed32972d9447540d5a047e60b9a2
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-07-08 21:20:26 +0200

    Added answer for exercise 2 (BigBrother).

--- src/main/java/za/co/entelect/forums/java/prac/task2/BigBrother.java
+++ src/main/java/za/co/entelect/forums/java/prac/task2/BigBrother.java
@@ -1,5 +1,12 @@
 package za.co.entelect.forums.java.prac.task2;
 
+import za.co.entelect.forums.java.example.App;
+import za.co.entelect.forums.java.example.domain.Vehicle;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class BigBrother {
 
     /*
@@ -8,4 +15,13 @@ public class BigBrother {
         App.getGantryA();
      */
 
+    public static void main(String[] args) {
+        List<Vehicle> vehicles = App.getGantryA().getVehicles();
+        Set<Vehicle> uniqueRegistrations = new HashSet<>();
+        uniqueRegistrations.addAll(vehicles);
+
+        for (Vehicle vehicle: uniqueRegistrations) {
+            System.out.println(vehicle.getRegistrationNumber());
+        }
+    }
 }
