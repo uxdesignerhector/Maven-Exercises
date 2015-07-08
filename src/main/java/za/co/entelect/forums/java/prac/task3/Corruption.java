3a2743c635e44fa318de51cff3b0c8c30d5b80cf
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-07-08 21:36:40 +0200

    Added answer for exercise 3 (Corruption).

--- src/main/java/za/co/entelect/forums/java/prac/task3/Corruption.java
+++ src/main/java/za/co/entelect/forums/java/prac/task3/Corruption.java
@@ -1,11 +1,39 @@
 package za.co.entelect.forums.java.prac.task3;
 
+import za.co.entelect.forums.java.example.App;
+import za.co.entelect.forums.java.example.domain.Gantry;
+import za.co.entelect.forums.java.example.domain.Vehicle;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Corruption {
 
     /*
-        Given a collection of Ganties, generate a list of unique vehicles with their total toll bill.
+        Given a collection of Gantries, generate a list of unique vehicles with their total toll bill.
 
         App.getGantryList();
      */
 
+    public static void main(String[] args) {
+        List<Gantry> gantries = App.getGantryList();
+        Map<Vehicle, BigDecimal> vehicleBill = new HashMap<>();
+
+        for (Gantry gantry : gantries) {
+            for (Vehicle vehicle : gantry.getVehicles()) {
+                if (!vehicleBill.containsKey(vehicle)) {
+                    vehicleBill.put(vehicle, new BigDecimal(gantry.getToll()));
+                } else {
+                    BigDecimal value = vehicleBill.get(vehicle);
+                    vehicleBill.put(vehicle, value.add(new BigDecimal(gantry.getToll())));
+                }
+            }
+        }
+
+        for (Vehicle vehicle : vehicleBill.keySet()) {
+            System.out.println(vehicle.getRegistrationNumber() + ": " + vehicleBill.get(vehicle));
+        }
+    }
 }
