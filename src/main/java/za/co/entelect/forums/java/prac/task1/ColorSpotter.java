f988f93131ed561320a88125aa536f71a9b71232
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-07-12 11:29:47 +0200

    Renamed ColourSpotter -> ColorSpotter for consistency. Renamed project in pom.xml. Added editorconfig.

--- src/main/java/za/co/entelect/forums/java/prac/task1/ColorSpotter.java
+++ src/main/java/za/co/entelect/forums/java/prac/task1/ColorSpotter.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 
-public class ColourSpotter {
+public class ColorSpotter {
 
     /*
         Given a Gantry, generate a map that groups the vehicles by colour.
